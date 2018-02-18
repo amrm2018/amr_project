2550ae3db35673b24776afc4a5ad6d6d11b6634b
amrm2018 <amr30848@gmail.com>
2018-02-18 15:00:28 +0200

    done desgin welcome screen + login + registration

---
+++ app/src/main/java/atfalna/atfalna/Home_Activity.java
@@ -0,0 +1,15 @@
+package atfalna.atfalna;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+}
---
+++ app/src/main/java/atfalna/atfalna/Registraion_Activity.java
@@ -0,0 +1,17 @@
+package atfalna.atfalna;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Registraion_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registraion);
+    }
+
+    public void btn_go_Login(View view) {
+    }
+}
