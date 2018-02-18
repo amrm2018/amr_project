ec3ae0d264cc3483c4e3ae542f1a660c1454cd8e
amrm2018 <amr30848@gmail.com>
2018-02-18 13:14:42 +0200

    Initial commit

---
+++ app/src/main/java/atfalna/atfalna/Login_Activity.java
@@ -0,0 +1,25 @@
+package atfalna.atfalna;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+    }
+
+    public void btn_login(View view) {
+    }
+
+    public void btn_go_Register(View view) {
+    }
+}
---
+++ app/src/main/java/atfalna/atfalna/Splash_Screen.java
@@ -0,0 +1,29 @@
+package atfalna.atfalna;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(getApplicationContext(), Login_Activity.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, 3000);
+    }
+}
