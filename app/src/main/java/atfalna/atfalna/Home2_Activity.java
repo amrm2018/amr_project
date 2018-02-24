06a0ae04484b86a2af6ac30cef41c825ead39d23
amrm2018 <amr30848@gmail.com>
2018-02-25 00:25:22 +0200

    Remember me login is Done

--- app/src/main/java/atfalna/atfalna/Home2_Activity.java
+++ app/src/main/java/atfalna/atfalna/Home2_Activity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import atfalna.atfalna.Login_Register_Java.Login2_Activity;
+
 public class Home2_Activity extends AppCompatActivity {
 
     @Override
@@ -20,5 +22,11 @@ public class Home2_Activity extends AppCompatActivity {
 
     public void relative_go_create_post(View view) {
         startActivity(new Intent(getApplicationContext(),Create_Post_Found_Activity.class));
+    }
+
+    public void btn_Logout(View view) {
+        getSharedPreferences("MyPref1",MODE_PRIVATE).edit().clear().commit();
+        startActivity(new Intent(getApplicationContext(),Login2_Activity.class));
+
     }
 }
--- app/src/main/java/atfalna/atfalna/Login_Register_Java/Login2_Activity.java
+++ app/src/main/java/atfalna/atfalna/Login_Register_Java/Login2_Activity.java
@@ -1,11 +1,15 @@
 package atfalna.atfalna.Login_Register_Java;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
@@ -21,12 +25,21 @@ import atfalna.atfalna.R;
 public class Login2_Activity extends AppCompatActivity {
 
     EditText Login_email , Login_password ;
-
+    CheckBox chk_remember;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+
+        SharedPreferences pref = getSharedPreferences("MyPref1",MODE_PRIVATE);
+        String email = pref.getString("email",null);
+        String password = pref.getString("password",null);
+        if (email !=null && password!=null)
+        {
+            startActivity(new Intent(getApplicationContext(),Home2_Activity.class));
+        }
+        chk_remember=findViewById(R.id.chk_remember);
 
         Login_email=findViewById(R.id.ed_email_log2);
         Login_password=findViewById(R.id.ed_pass_log2);
@@ -50,8 +63,8 @@ public class Login2_Activity extends AppCompatActivity {
     //Login
     public void Login_btn_Login (View v){
 
-        String Log_in_email=Login_email.getText().toString().trim();
-        String Log_in_password=Login_password.getText().toString().trim();
+        final String Log_in_email=Login_email.getText().toString().trim();
+        final String Log_in_password=Login_password.getText().toString().trim();
 
 
         Response.Listener<String>responseListener = new Response.Listener<String>() {
@@ -64,6 +77,16 @@ public class Login2_Activity extends AppCompatActivity {
                     boolean success = jsonObject.getBoolean("success");
                     if (success){
                         Toast.makeText(Login2_Activity.this, "تم تسجيل الدخول", Toast.LENGTH_SHORT).show();
+                        if (chk_remember.isChecked()) {
+                            getSharedPreferences("MyPref1", MODE_PRIVATE)
+                                    .edit().putString("email", Log_in_email).putString("password", Log_in_password).apply();
+                            // OR
+//                                        SharedPreferences shrd =  getSharedPreferences("MyPref", Context.MODE_PRIVATE);
+//                                        SharedPreferences.Editor editor= shrd.edit();
+//                                        editor.putString("email",Log_in_name);
+//                                        editor.putString("password",Log_in_password);
+//                                        editor.apply();
+                        }
                         startActivity(new Intent(getApplicationContext(),Home2_Activity.class));
                     }else {
                         Toast.makeText(Login2_Activity.this, "البيانات غير صحيحة", Toast.LENGTH_SHORT).show();
