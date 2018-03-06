1eec8214247b1a35c15e7dba292cdc0ae32917c3
amrm2018 <amr30848@gmail.com>
2018-03-07 01:33:12 +0200

    2- تم تجهيز الشغل لشاشة ال Post missing + class send data

--- app/src/main/java/atfalna/atfalna/Show_All_Post_Found/Show_All_Found_Activity.java
+++ app/src/main/java/atfalna/atfalna/Show_All_Post_Found/Show_All_Found_Activity.java
@@ -142,7 +142,7 @@ public class Show_All_Found_Activity extends AppCompatActivity {
             datetime.setText(listA.get(i).date_time_p);
            // phone.setText(listA.get(i).phone);
 
-            Picasso.with(getApplicationContext()).load("http://192.168.1.3/app_atfalna/img_atfalna/"+listA.get(i).img_f).into(imgfound);
+            Picasso.with(getApplicationContext()).load("http://192.168.1.3/app_atfalna/img_found/"+listA.get(i).img_f).into(imgfound);
 
             imgfound.setOnClickListener(new View.OnClickListener() {
                 @Override
